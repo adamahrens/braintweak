69a503adf650a31b0b2c373fa47996d11fee7f89
Adam Ahrens <adam.ahrens@smartthings.com>
2015-02-20 09:30:48 -0600

    Initial project commit

---
+++ BrainTweak/BrainTweak/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  BrainTweak
+//
+//  Created by Adam Ahrens on 2/20/15.
+//  Copyright (c) 2015 Appsbyahrens. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
